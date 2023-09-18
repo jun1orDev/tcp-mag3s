635087906aba88acca7a615ee7f3985c90d8254d
jun1orDev <cabral_junior@icloud.com>
2023-09-18 12:58:54 -0300

    estrutura inicial do projeto

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },  
+
+  modules: [
+    "@pinia/nuxt"
+  ],
+
+  imports: {
+    dirs: ['.stores'],
+  },
+
+  pinia: {
+    autoImports: ['defineStore']
+  }
+})
