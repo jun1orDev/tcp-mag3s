f6747bb9442432a7b6a7dc1585cdfc2fdd6d9181
Gabr1elle <gabrielle.santos@mag3s.com>
2023-09-26 16:29:14 -0300

    Editando tela de loading

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,13 +1,15 @@
 <template>
-	<div>
-		<h1>loading</h1>
+	<div class="flex flex-col justify-center items-center bg-cover absolute bg-slate-950 top-0 right-0 left-0 bottom-0 bg-no-repeat bg-center bg-[url('/imgs/img_fundo_loading.png')]">
+		<img src="/imgs/img_loading.png" class="mb-60 w-60 md:w-auto" >
+		<img src="/imgs/img_fiel_da_sorte.png">
 	</div>
+
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,7 +32,7 @@ export default defineNuxtConfig({
 
 	ui: {
 		global: true,
-		icons: ['material-symbols'],
+		icons: ['material-symbols', 'svg-spinners'],
 	},
 
 	imports: {
