afa5e897cc31ef6dd74c5133c517b87ce1028dce
jun1orDev <cabral_junior@icloud.com>
2023-09-27 10:50:59 -0300

    removendo testes realizados na tela inicial

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,9 @@
 <template>
   <div>
-    <p class="text-4xl text-amber-700">página inicial</p>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <ul>
-     <li v-for="user in users">{{ user }}</li>
-    </ul>
   </div>
 </template>
 
 <script setup>
-const { data: users } = await useFetch('/api/users')
 </script>
 
 <style>
