10bd99415678a778cc21df61f7534d7c94b66122
Gabr1elle <gabrielle.santos@mag3s.com>
2023-09-27 10:30:21 -0300

    iniciando a criação da tela de ultimos sorteios

---
+++ components/BannerCard.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class=" flex justify-center items-center bg-cover absolute bg-slate-950 top-0 right-0 left-0 bottom-0 bg-no-repeat bg-center bg-[url('/imgs/img_fundo_loading.png')]">
+		<img src="imgs/card_sorteio_atual_mobile.png">
+
+	</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
