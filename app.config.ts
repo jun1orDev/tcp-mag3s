4d2ba27391b65e8f97d08101c79be42ce8850821
Gabr1elle <gabrielle.santos@mag3s.com>
2023-09-28 17:09:38 -0300

    alterando informações no código

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,6 @@
 export default defineAppConfig({
 	ui: {
 		primary: 'blue',
-		border: '#847248',
 		gray: 'cool',
 		notifications: {
 			// Show toasts at the top right of the screen
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,13 +20,6 @@ export default {
 		},
 		extend: {
 		},
-		color: {
-			transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-			'dourado': "#847248",
-		}
 	},
 	plugins: [],
 };
