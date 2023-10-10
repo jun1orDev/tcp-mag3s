d6b78fd06b2099c6d782e1f2b68bdea1c4f8d370
Gabr1elle <gabrielle.santos@mag3s.com>
2023-10-10 17:15:50 -0300

    Atualizando a segunda página de numeros dos sorteios

---
+++ components/NumeroSorteio.vue
@@ -0,0 +1,22 @@
+<template>
+	<div
+		class="cor h-12 w-12 rounded-xl flex justify-center items-center border-black-2" :class="props.customBackground"
+	>
+		<div class="button text-[20px] text-white">
+			<span>{{ props.button }}</span>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const props = defineProps(['button', 'customBackground']);
+</script>
+
+<style scoped>
+.button {
+	font-family: 'Gotham Black';
+}
+.cor{
+	background: #DFA701;
+}
+</style>
--- pages/detalhes-premios/index.vue
+++ pages/detalhes-premios/index.vue
@@ -1,7 +1,6 @@
 <template>
 	<div>
 		<DetalhesPremio/>
-
 	</div>
 </template>
 
