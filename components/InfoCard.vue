d1a5cb626dc1748b5d7ac28ee7013faf02db2a4a
Jun1orDev <junior.araujo@mag3s.com>
2023-10-10 14:37:31 -0300

    alguns ajustes gerais

--- components/InfoCard.vue
+++ components/InfoCard.vue
@@ -1,37 +1,53 @@
 <template>
-  <div class="campo relative" :style="props.customBackground">
-    <div>
-      <UTextarea class="texto" color="yellow" variant="outline" :rows="3" disabled />
-      <img :src="props.imagemSrc" class="absolute scale-2 top-0 left-0 object-cover z-30 -mt-4" />
-    </div>
-    <div class="text-amber-50 z-10 absolute top-0 left-0">
-      <div>
-        <p class="ml-28 mb-1 mt-6 text-xs md:text-sm texto-principal">
-          <span class="text-slate-200">{{ props.titulo }}</span>
-        </p>
-        <p class="ml-28 -mt-1 text-[10px] md:text-sm sub">
-          <span class="text-slate-200">{{ props.subtitulo }}</span>
-        </p>
-      </div>
-    </div>
-  </div>
+	<NuxtLink :to="props.source" class="relative w-full flex justify-between border border-amber-400 rounded-xl" :class="props.customBackground ? 'gradiente': 'bg-black'">
+		<div class="flex items-center text-white">
+			<!-- imagem -->
+			<div class="mx-5">
+				<img :src="props.imagemSrc" class="scale-150 top-0 left-0 object-cover w-[98px]" />
+			</div>
+
+			<!-- Data -->
+			<div class="text-center me-5">
+				<p class="number-data text-[26px] text-md-[30px] leading-5 md:leading-6">{{props.date.day}}</p>
+				<p class="uppercase text-base">{{props.date.month}}</p>
+			</div>
+
+			<!-- Titulo e subtítulo -->
+			<div>
+				<div>
+					<p class="text-xs md:text-sm texto-principal leading-3 md:leading-normal uppercase">
+						<span>{{ props.titulo }}</span>
+					</p>
+					<p class="text-[10px] md:text-sm sub leading-3 md:leading-normal">
+						<span>{{ props.subtitulo }}</span>
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<!-- Botão -->
+		<div class="text-white flex items-center text-xl md:text-3xl rounded-e-xl" style="background-color: #827248;">
+			<UIcon class="mx-1" name="i-material-symbols-arrow-forward-ios" />
+		</div>
+	</NuxtLink>
 </template>
 
 <script setup>
-const props = defineProps(['titulo', 'subtitulo', 'imagemSrc', 'customBackground']);
+const props = defineProps(['titulo', 'subtitulo', 'imagemSrc', 'source', 'customBackground', 'date']);
 </script>
 
-<style>
-.campo {
-	border-radius: 5px;
-}
-.texto {
+<style scoped>
+.texto-principal,
+.texto,
+.number-data {
 	font-family: 'Gotham Black';
 }
-.texto-principal {
-	font-family: 'Gotham Black';
-}
+
 .sub {
 	font-family: 'Gotham Medium';
+}
+
+.gradiente {
+	background: linear-gradient(90deg, #DFA701 -15.84%, rgba(0, 0, 0, 0.70) 77.28%);
 }
 </style>
