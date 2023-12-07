f935341aa14330174665b4ba67a6adb24f3b838e
Gabr1elle <gabrielle.santos@mag3s.com>
2023-12-07 11:33:32 -0300

    Ajustes Finais - Tela Regulamento.

--- pages/regulamento/index.vue
+++ pages/regulamento/index.vue
@@ -1,16 +1,37 @@
 <template>
 	<div>
 		<AppLayoutBgDefault />
-		<UContainer class="pb-36">
-			<div class="py-6">
-				<h1 class="title uppercase text-[#847248] text-[12px] md:text-[22px]">
-					{{ store.contentApp.sessions_title_five }}
-				</h1>
-			</div>
-			<div class="rules flex justify-center pb-8">
-				<p class="text-white text-[9px] md:text-[20px] text-justify">
-					{{ store.contentApp.sessions_subtitle_nine }}
-				</p>
+
+		<UContainer>
+			<div
+				class="flex items-center justify-center min-h-screen pt-14 lg:pt-24 pb-10"
+			>
+				<div
+					class="lg:grid-cols-[400px_1fr] gap-8 lg:gap-24 items-start justify-center w-screen"
+					:class="classGrid"
+				>
+					<!-- Imagem do Brand -->
+					<AppOthersImageBrand />
+
+					<div class="flex flex-col justify-center">
+						<div class="py-6">
+							<h1
+								class="fm3 uppercase text-[12px] md:text-[22px]"
+								:style="`color: ${store.contentApp.colors_text_two}`"
+							>
+								{{ store.contentApp.sessions_title_five }}
+							</h1>
+						</div>
+						<div class="fm2 flex justify-center pb-8">
+							<p
+								class="text-[9px] md:text-[20px] text-justify"
+								:style="`color: ${store.contentApp.colors_text_one}`"
+							>
+								{{ store.contentApp.sessions_subtitle_nine }}
+							</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</UContainer>
 	</div>
@@ -19,14 +40,13 @@
 <script setup>
 import { useStoreApp } from '~/stores/app';
 const store = useStoreApp();
+const app = useStoreApp().contentApp;
 
+const classGrid = computed(() => {
+	return {
+		grid: app.config_will_have_image_brand_session_hotsite,
+	};
+});
 </script>
 
-<style scoped>
-.title {
-	font-family: 'Gotham Black';
-}
-.rules {
-	font-family: 'Gotham Book';
-}
-</style>
+<style scoped></style>
