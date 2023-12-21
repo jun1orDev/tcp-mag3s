530aa5d0c2f29ca732235e770a1408438c71a7d5
jun1orDev <cabral_junior@icloud.com>
2023-12-21 14:50:27 -0300

    utilizando a variável da imagem de fundo para login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,8 +1,8 @@
 <template>
 	<!-- BG padrão de fundo da página -->
 	<div class="absolute">
-		<LpBgDefault v-if="app.config_will_have_hotsite" :image="app.layout_background_app_two"
-			:imageMobile="app.layout_background_app_mobile_two" placeholderImage="/imgs/lp/cover_banner_3.png"
+		<LpBgDefault v-if="app.config_will_have_hotsite" :image="bgImage"
+			:imageMobile="bgImageMobile" placeholderImage="/imgs/lp/cover_banner_3.png"
 			placeholderImageMobile="/imgs/lp/cover_banner_mobile_3.png" :bgColor="app.layout_background_colors_app_two"
 			position="fixed" />
 		<AppLayoutBgDefault v-else />
@@ -46,6 +46,14 @@ const { pathAssets } = useRuntimeConfig().public;
 
 definePageMeta({
 	middleware: ['login-app']
+});
+
+const bgImage = computed(() => {
+	return `${pathAssets}${app.layout_background_app_two}`;
+});
+
+const bgImageMobile = computed(() => {
+	return `${pathAssets}${app.layout_background_app_mobile_two}`;
 });
 
 const textColor = computed(() => {
