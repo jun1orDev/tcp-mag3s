0a1857dc2644e0073851cedd58ccae3baf8a4ae0
Gabr1elle <gabrielle.santos@mag3s.com>
2024-01-09 16:38:16 -0300

    Ajustes no brand - termos

--- pages/termos/index.vue
+++ pages/termos/index.vue
@@ -1,29 +1,57 @@
 <template>
 	<!-- BG padrão de fundo da página -->
 	<div class="absolute">
-		<LpBgDefault v-if="app.config_will_have_hotsite" :image="app.layout_background_app_two" :imageMobile="app.layout_background_app_mobile_two" placeholderImage="/imgs/lp/cover_banner_3.png"
-		placeholderImageMobile="/imgs/lp/cover_banner_mobile_3.png" :bgColor="app.layout_background_colors_app_two" position="fixed" />
+		<LpBgDefault
+			v-if="app.config_will_have_hotsite"
+			:image="app.layout_background_app_two"
+			:imageMobile="app.layout_background_app_mobile_two"
+			placeholderImage="/imgs/lp/cover_banner_3.png"
+			placeholderImageMobile="/imgs/lp/cover_banner_mobile_3.png"
+			:bgColor="app.layout_background_colors_app_two"
+			position="fixed"
+		/>
 		<AppLayoutBgDefault v-else />
 	</div>
 
-	<AppLayoutHeader v-if="app.config_will_have_hotsite" :hasLogout="false" :bgColor="app.header_colors_background_app_two" :textColor="app.header_colors_text_app" :isLogoDark="true" />
+	<AppLayoutHeader
+		v-if="app.config_will_have_hotsite"
+		:hasLogout="false"
+		:bgColor="app.header_colors_background_app_two"
+		:textColor="app.header_colors_text_app"
+		:isLogoDark="true"
+	/>
 
-	<UContainer class="flex justify-center min-h-screen pt-14 lg:pt-24 pb-52 lg:pb-80" :class="isItemsCenter">
-		<div class="lg:grid-cols-[400px_1fr] gap-8 lg:gap-16 justify-center items-start w-screen" :class="isGridLayout">
+	<UContainer
+		class="flex justify-center min-h-screen pt-14 lg:pt-24 pb-52 lg:pb-80"
+		:class="isItemsCenter"
+	>
+		<div
+			class="lg:grid-cols-[400px_1fr] gap-8 lg:gap-16 justify-center items-start w-screen"
+			:class="isGridLayout"
+		>
 			<!-- Imagem do Brand -->
-			<AppOthersImageBrandSession />
+			<div class="hidden lg:block md:flex justify-center">
+				<AppOthersImageBrandSession />
+			</div>
+
+			<!-- titulo e subtitulo -->
 
 			<div class="flex flex-col justify-center">
 				<div>
-					<h1 class="fm3 uppercase text-[12px] md:text-[22px]" :style="`color: ${app.colors_text_two}`">
+					<h1
+						class="fm3 uppercase text-[12px] md:text-[22px]"
+						:style="`color: ${app.colors_text_two}`"
+					>
 						{{ app.terms_and_condition_text_title }}
 					</h1>
 				</div>
 
 				<div class="fm2">
-					<p class="text-[9px] md:text-[20px] text-justify" :style="`color: ${app.colors_text_one}`"
-						v-html="app.terms_and_condition_text_document">
-					</p>
+					<p
+						class="text-[9px] md:text-[20px] text-justify"
+						:style="`color: ${app.colors_text_one}`"
+						v-html="app.terms_and_condition_text_document"
+					></p>
 				</div>
 			</div>
 		</div>
@@ -32,19 +60,32 @@
 	<!-- footer politica e logos -->
 	<UContainer class="fixed bottom-0 left-0 right-0 px-0 md:px-4">
 		<div class="py-6 px-4 bg-cover" :style="background">
-
 			<div class="max-w-[600px] m-auto">
-				<p class="text-center text-[9px] md:text-[12px]" :style="`color: ${app.colors_text_one}`">
+				<p
+					class="text-center text-[9px] md:text-[12px]"
+					:style="`color: ${app.colors_text_one}`"
+				>
 					{{ app.terms_and_condition_text_policy }}
 				</p>
 			</div>
 			<div class="flex items-center justify-center mt-5">
 				<div class="w-[150px] md:w-auto">
-					<img :src="imageLogo" onerror="this.src='/imgs/img_fiel_da_sorte.png'" class="mx-auto" />
+					<img
+						:src="imageLogo"
+						onerror="this.src='/imgs/img_fiel_da_sorte.png'"
+						class="mx-auto"
+					/>
 				</div>
-				<div class="h-10 border-l-2 mx-5" :style="`color: ${app.colors_text_one}`"></div>
+				<div
+					class="h-10 border-l-2 mx-5"
+					:style="`color: ${app.colors_text_one}`"
+				></div>
 				<div class="w-[100px] md:w-auto">
-					<img :src="imageLogoTwo" onerror="this.src='/imgs/fiel.png'" class="mx-auto" />
+					<img
+						:src="imageLogoTwo"
+						onerror="this.src='/imgs/fiel.png'"
+						class="mx-auto"
+					/>
 				</div>
 			</div>
 		</div>
@@ -72,7 +113,7 @@ const imageLogoTwo = computed(() => {
 
 const isGridLayout = computed(() => {
 	return {
-		'grid': app.config_will_have_image_brand_session_hotsite,
+		grid: app.config_will_have_image_brand_session_hotsite,
 	};
 });
 
