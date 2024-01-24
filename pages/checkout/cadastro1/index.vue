3281d6ab40ec2ef49a0d1b602ecf8c8dde7e9c9d
Gabr1elle <gabrielle.santos@mag3s.com>
2024-01-24 17:23:56 -0300

    Implementar validação do input utilizando Yup/ possibilidade do usuário ver a senha que digita.

--- pages/checkout/cadastro1/index.vue
+++ pages/checkout/cadastro1/index.vue
@@ -2,44 +2,123 @@
 	<div class="max-w-[600px] m-auto lg:w-full lg:m-0">
 		<div>
 			<h1 v-html="titleText" class="fm3 text-xl lg:text-2xl mb-1"></h1>
-			<p class="fm2 text-lg">{{ descriptionText }} <ULink type="link" :to="{path: '/checkout/login', query: {idPkg: storeCheckout.packageChosen.id, idOB: storeCheckout.packageChosenOB.id}}" class="fm3">Clique aqui</ULink>
-				.</p>
+			<p class="fm2 text-lg">
+				{{ descriptionText }}
+				<ULink
+					type="link"
+					:to="{
+						path: '/checkout/login',
+						query: {
+							idPkg: storeCheckout.packageChosen.id,
+							idOB: storeCheckout.packageChosenOB.id,
+						},
+					}"
+					class="fm3"
+					>Clique aqui</ULink
+				>
+				.
+			</p>
 		</div>
 
 		<!-- Primeira parte do Cadastro -->
-		<UForm id="formRegisterOne" :state="storeCheckout.formRegister" class="space-y-4 mt-6"
-			@submit="storeCheckout.registerEmail(useToast, storeCheckout.packageChosen.id, storeCheckout.packageChosenOB.id, '/checkout/cadastro2')">
+		<UForm
+			id="formRegisterOne"
+			:state="storeCheckout.formRegister"
+			:schema="schema"
+			class="space-y-4 mt-6"
+			@submit="
+				storeCheckout.registerEmail(
+					useToast,
+					storeCheckout.packageChosen.id,
+					storeCheckout.packageChosenOB.id,
+					'/checkout/cadastro2'
+				)
+			"
+		>
 			<UFormGroup label="E-mail:" name="email">
-				<UInput size="xl" v-model="storeCheckout.formRegister.email" type="email" color="white" variant="outline"
-					:ui="configInput" icon="i-material-symbols-stacked-email-outline" />
+				<UInput
+					size="xl"
+					v-model="storeCheckout.formRegister.email"
+					type="email"
+					color="white"
+					variant="outline"
+					:ui="configInput"
+					icon="i-material-symbols-stacked-email-outline"
+				/>
 			</UFormGroup>
 
 			<UFormGroup label="Senha:" name="password">
-				<UInput size="xl" v-model="storeCheckout.formRegister.password" type="password" color="white" :ui="configInput"
-					icon="i-material-symbols-passkey-outline-rounded" />
+				<UInput
+					size="xl"
+					v-model="storeCheckout.formRegister.password"
+					color="white"
+					:type="passView.password"
+					:ui="{ ...configInput, icon: { trailing: { pointer: '' } } }"
+					icon="i-material-symbols-passkey-outline-rounded"
+				/>
+				<template #trailing>
+					<UButton
+						v-show="storeCheckout.resetUser.password !== ''"
+						color="gray"
+						variant="link"
+						:icon="passIcon.password"
+						:padded="false"
+						@click="togglePassView('password')"
+					/>
+				</template>
 			</UFormGroup>
 
-			<UFormGroup label="Confirme a senha:" name="password-confirm">
-				<UInput size="xl" v-model="storeCheckout.formRegister.confirmPassword" type="password" color="white"
-					:ui="configInput" icon="i-material-symbols-passkey-outline-rounded" />
+			<UFormGroup label="Confirme a senha:" name="confirmPassword">
+				<UInput
+					size="xl"
+					v-model="storeCheckout.formRegister.confirmPassword"
+					color="white"
+					:type="passView.confirmPassword"
+					:ui="{ ...configInput, icon: { trailing: { pointer: '' } } }"
+					icon="i-material-symbols-passkey-outline-rounded"
+				/>
+				<template #trailing>
+					<UButton
+						v-show="storeCheckout.formRegister.confirmPassword !== ''"
+						color="gray"
+						variant="link"
+						:icon="passIcon.confirmPassword"
+						:padded="false"
+						@click="togglePassView('confirmPassword')"
+					/>
+				</template>
 			</UFormGroup>
 
-			<UCheckbox label="Termos de uso"
+			<UCheckbox
+				label="Termos de uso"
 				help="Li e aceito os regulamentos e concordo com a política de privacidade e termos de uso."
-				v-model="storeCheckout.formRegister.terms" :ui="configCheckbox" required />
+				v-model="storeCheckout.formRegister.terms"
+				:ui="configCheckbox"
+				required
+			/>
 
 			<div class="flex justify-center">
-				<UButton size="xl" label="continuar" type="submit" :ui="configButton" :style="[colorBgButton, colorTextButton]"
-					class="fm3" :loading="storeCheckout.formRegister.loading" trailing :disabled="!storeCheckout.enableButtonNextOne" />
+				<UButton
+					size="xl"
+					label="continuar"
+					type="submit"
+					:ui="configButton"
+					:style="[colorBgButton, colorTextButton]"
+					class="fm3"
+					:loading="storeCheckout.formRegister.loading"
+					trailing
+					:disabled="!storeCheckout.enableButtonNextOne"
+				/>
 			</div>
 		</UForm>
-
 	</div>
 </template>
 
 <script setup>
 import { useStoreApp } from '~/stores/app';
 import { useStoreCheckout } from '~/stores/checkout';
+import { object, string } from 'yup';
+import * as Yup from 'yup';
 
 const store = useStoreApp();
 const app = store.contentApp;
@@ -48,7 +127,49 @@ const { pathAssets } = useRuntimeConfig().public;
 
 definePageMeta({
 	layout: 'checkout-default',
-	middleware: ['auth-client', 'purchase-list-packages', 'purchase-chosen-package', 'redirect-auth-user-register-one']
+	middleware: [
+		'auth-client',
+		'purchase-list-packages',
+		'purchase-chosen-package',
+		'redirect-auth-user-register-one',
+	],
+});
+
+const passView = reactive({
+	password: 'password',
+	confirmPassword: 'password',
+});
+
+const passIcon = reactive({
+	password: 'i-material-symbols-visibility-rounded',
+	confirmPassword: 'i-material-symbols-visibility-rounded',
+});
+
+function togglePassView(view) {
+	switch (passView[view]) {
+		case 'password':
+			passView[view] = 'text';
+			passIcon[view] = 'i-material-symbols-visibility-off-rounded';
+			break;
+		default:
+			passView[view] = 'password';
+			passIcon[view] = 'i-material-symbols-visibility-rounded';
+			break;
+	}
+}
+
+const schema = object({
+	email: string().email('E-mail inválido').required('Campo obrigatório'),
+	password: string()
+		.min(8, 'Mínimo de 8 caracteres')
+		.matches(
+			/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/,
+			'A senha deve conter pelo menos um número, uma letra maiúscula e uma letra minúscula'
+		)
+		.required('Campo obrigatório'),
+	confirmPassword: string()
+		.oneOf([Yup.ref('password')], 'As senhas não coincidem')
+		.required('Campo obrigatório'),
 });
 
 const textColor = computed(() => {
@@ -71,7 +192,8 @@ onMounted(() => {
 			{ step: 3, complete: false },
 		],
 		16,
-		true);
+		true
+	);
 });
 
 const colorBgButton = computed(() => {
@@ -83,21 +205,20 @@ const colorTextButton = computed(() => {
 });
 
 const configInput = ref({
-	"rounded": 'rounded-full',
+	rounded: 'rounded-full',
 });
 
 const configButton = ref({
-	"rounded": 'rounded-full',
-	"padding": {
-		"xl": "px-20 py-2"
+	rounded: 'rounded-full',
+	padding: {
+		xl: 'px-20 py-2',
 	},
 });
 
 const configCheckbox = ref({
-	"label": "text-terms fm3",
-	"help": "text-sm text-terms fm2",
+	label: 'text-terms fm3',
+	help: 'text-sm text-terms fm2',
 });
-
 </script>
 
 <style>
