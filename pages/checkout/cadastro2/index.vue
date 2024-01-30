8fbd46dd288e7e91fa65507e4b68b71490cb8b64
Gabr1elle <gabrielle.santos@mag3s.com>
2024-01-30 14:48:40 -0300

    Implementando validação dos inputs utilizando Yup

--- pages/checkout/cadastro2/index.vue
+++ pages/checkout/cadastro2/index.vue
@@ -6,36 +6,79 @@
 		</div>
 
 		<!-- Segunda parte do Cadastro -->
-		<UForm id="formRegisterTwo" :validate="validate" :state="state" class="space-y-4 mt-6"
-			@submit="storeCheckout.registerOthersDatas(useToast, storeCheckout.packageChosen.id, storeCheckout.packageChosenOB.id, '/checkout/pagamento')">
+		<UForm
+			id="formRegisterTwo"
+			:validate="validate"
+			:state="storeCheckout.formRegister"
+			:schema="schema"
+			class="space-y-4 mt-6"
+			@submit="
+				storeCheckout.registerOthersDatas(
+					useToast,
+					storeCheckout.packageChosen.id,
+					storeCheckout.packageChosenOB.id,
+					'/checkout/pagamento'
+				)
+			"
+		>
 			<UFormGroup label="Nome completo:" name="name">
-				<UInput size="xl" v-model="storeCheckout.formRegister.name" type="text" color="white" variant="outline"
-					:ui="configInput" icon="i-material-symbols-person-outline" />
+				<UInput
+					size="xl"
+					v-model="storeCheckout.formRegister.name"
+					type="text"
+					color="white"
+					variant="outline"
+					:ui="configInput"
+					icon="i-material-symbols-person-outline"
+				/>
 			</UFormGroup>
 
 			<UFormGroup label="Telefone:" name="phone">
-				<UInput size="xl" type="tel" color="white" :ui="configInput" v-maska:[optionsMaskPhone]
-					data-maska="['(##) #####-####', '(##) ####-####']" icon="i-material-symbols-add-call-outline-rounded" />
+				<UInput
+					size="xl"
+					type="tel"
+					color="white"
+					:ui="configInput"
+					v-maska:[optionsMaskPhone]
+					data-maska="['(##) #####-####', '(##) ####-####']"
+					icon="i-material-symbols-add-call-outline-rounded"
+				/>
 			</UFormGroup>
 
 			<UFormGroup label="CPF:" name="cpf">
-				<UInput size="xl" v-model="storeCheckout.formRegister.cpf" type="text" color="white" :ui="configInput" v-maska
-					data-maska="###.###.###-##" icon="i-material-symbols-wallet" />
+				<UInput
+					size="xl"
+					v-model="storeCheckout.formRegister.cpf"
+					type="text"
+					color="white"
+					:ui="configInput"
+					v-maska
+					data-maska="###.###.###-##"
+					icon="i-material-symbols-wallet"
+				/>
 			</UFormGroup>
 
 			<div class="flex justify-center">
-				<UButton size="xl" label="continuar para pagamento" type="submit" :ui="configButton"
-					:style="[colorBgButton, colorTextButton]" class="fm3 mt-6" :loading="storeCheckout.formRegister.loading"
-					trailing :disabled="!storeCheckout.enableButtonNextTwo" />
+				<UButton
+					size="xl"
+					label="continuar para pagamento"
+					type="submit"
+					:ui="configButton"
+					:style="[colorBgButton, colorTextButton]"
+					class="fm3 mt-6"
+					:loading="storeCheckout.formRegister.loading"
+					trailing
+					:disabled="!storeCheckout.enableButtonNextTwo"
+				/>
 			</div>
 		</UForm>
-
 	</div>
 </template>
 
 <script setup>
 import { useStoreApp } from '~/stores/app';
 import { useStoreCheckout } from '~/stores/checkout';
+import { object, string} from 'yup';
 
 const store = useStoreApp();
 const app = store.contentApp;
@@ -44,12 +87,23 @@ const { pathAssets } = useRuntimeConfig().public;
 
 definePageMeta({
 	layout: 'checkout-default',
-	middleware: ['auth-client', 'purchase-list-packages', 'purchase-chosen-package', 'redirect-auth-user-register-two']
+	middleware: [
+		'auth-client',
+		'purchase-list-packages',
+		'purchase-chosen-package',
+		'redirect-auth-user-register-two',
+	],
+});
+
+const schema = object({
+	name: string().required('O campo nome é obrigatório'),
+	phone: string().required('O campo telefone é obrigatório'),
+	cpf: string().required('O campo CPF é obrigatório'),
 });
 
 const optionsMaskPhone = ref({
-	onMaska: (detail) => storeCheckout.formRegister.phone = detail.unmasked
-})
+	onMaska: (detail) => (storeCheckout.formRegister.phone = detail.unmasked),
+});
 
 const titleText = computed(() => {
 	return `${app.create_user_text_title_two}`;
@@ -67,7 +121,8 @@ onMounted(() => {
 			{ step: 3, complete: false },
 		],
 		35,
-		true);
+		true
+	);
 });
 
 const colorBgButton = computed(() => {
@@ -79,13 +134,13 @@ const colorTextButton = computed(() => {
 });
 
 const configInput = ref({
-	"rounded": 'rounded-full',
+	rounded: 'rounded-full',
 });
 
 const configButton = ref({
-	"rounded": 'rounded-full',
-	"padding": {
-		"xl": "px-12 py-2"
+	rounded: 'rounded-full',
+	padding: {
+		xl: 'px-12 py-2',
 	},
 });
 </script>
