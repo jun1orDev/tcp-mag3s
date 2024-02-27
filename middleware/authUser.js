c6192030eee9739be14a982469218ee1de73c9c8
jun1orDev <cabral_junior@icloud.com>
2024-02-27 12:43:36 -0300

    inserindo feedback de usuário navegando e fazendo login

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -5,11 +5,21 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 	const app = useNuxtApp();
 	const storeApp = useStoreApp(app.$pinia);
 	const storeIncentive = useStoreIncentive(app.$pinia);
+	const toast = useToast();
 
 	// Caso a aplicação tenha autenticação obrigatória do usuário redirecionar para a tela de Login
 	if (storeApp.contentApp.config_will_have_hotsite) {
 		if (!useCookie('tokenUser').value) {
-			console.log('sem cookie de autenticação, refaça o login');
+			console.log('sem cookie de autenticação, refaça o login.');
+			toast.add({
+				id: 'show_status_login_access_error',
+				color: `red`,
+				title: `Atenção!`,
+				description: `Autenticação expirada, refaça o login.`,
+				icon: `i-material-symbols-warning-outline-rounded`,
+				timeout: 3500,
+			});
+
 			storeIncentive.loading = true;
 			return navigateTo({ path: '/login' });
 		}
