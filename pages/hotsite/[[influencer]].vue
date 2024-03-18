ac39815a9d7a08c9a0df5144eb70a29aaed22ce8
Gabrielle Santos <gabrielle.santos@mag3s.com>
2024-03-18 17:29:01 -0300

    Inserir um toast com um botão para aceitar o uso de cookies

--- pages/hotsite/[[influencer]].vue
+++ pages/hotsite/[[influencer]].vue
@@ -10,9 +10,9 @@
 		<LpStepsRegister />
 
 		<!-- Sesstions -->
-		<LpSession v-if="app.config_will_have_scratch_card" :title="app.session_text_title_hotsite_one" :description="app.session_text_description_hotsite_one"
-			:image="app.session_image_hotsite_one" :bgImage="bgImageSessionOne" :bgImageMobile="bgMobileImageSessionOne"
-			positionText="left" positionImage="right" />
+		<LpSession v-if="app.config_will_have_scratch_card" :title="app.session_text_title_hotsite_one"
+			:description="app.session_text_description_hotsite_one" :image="app.session_image_hotsite_one"
+			:bgImage="bgImageSessionOne" :bgImageMobile="bgMobileImageSessionOne" positionText="left" positionImage="right" />
 
 		<LpSession :title="app.session_text_title_hotsite_two" :description="app.session_text_description_hotsite_two"
 			:image="app.session_image_hotsite_two" :bgImage="bgImageSessionTwo" :bgImageMobile="bgMobileImageSessionTwo"
@@ -31,10 +31,28 @@
 
 <script setup>
 import { useStoreApp } from '~/stores/app';
+
 const store = useStoreApp();
 const app = useStoreApp().contentApp;
-
 const { pathAssets } = useRuntimeConfig().public;
+const toast = useToast();
+
+
+//  Método para exibir o toast
+onMounted(() => {
+	toast.add({
+		id: 'cookies',
+		title: 'Cookies',
+		description: 'Nós utilizamos cookies para melhorar a sua experiência neste site. Ao confirmar, você concorda com o nosso uso de cookies.',
+		icon: 'i-material-symbols:cookie-outline-rounded',
+		timeout: 35000,
+		actions: [{
+			label: 'Confirmar',
+			click: () => {
+			}
+		}]
+	})
+});
 
 // Seção 1
 const bgMobileImageSessionOne = computed(() => {
