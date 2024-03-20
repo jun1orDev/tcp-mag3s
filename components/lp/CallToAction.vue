e9a92f4eda267d9602a7e4f658fc2ebf1b58fc8d
Jun1orDev <junior.araujo@mag3s.com>
2024-03-20 20:05:28 -0300

    inserindo o loading no botão de calltoaction principal do hotsite

--- components/lp/CallToAction.vue
+++ components/lp/CallToAction.vue
@@ -1,10 +1,12 @@
 <template>
-  <UButton to="/checkout/pacotes" :label="labelButton" :ui="{ rounded: 'rounded-full' }" block size="xl" :style="[colorBgButton, colorTextButton]" class="fm3 fixed bottom-6 lg:bottom-10 left-1/2 !-translate-x-1/2 max-w-[80%] md:max-w-xl uppercase animate__animated animate__fadeInDown" />
+  <UButton to="/checkout/pacotes" :label="labelButton" :ui="{ rounded: 'rounded-full' }" block size="xl" :style="[colorBgButton, colorTextButton]" class="fm3 fixed bottom-6 lg:bottom-10 left-1/2 !-translate-x-1/2 max-w-[80%] md:max-w-xl uppercase animate__animated animate__fadeInDown" :loading="storeCheckout.packageLoading" trailing />
 </template>
 
 <script setup>
 import { useStoreApp } from '~/stores/app';
+import { useStoreCheckout } from '~/stores/checkout';
 const app = useStoreApp().contentApp;
+const storeCheckout = useStoreCheckout();
 
 const labelButton = ref('Quero participar');
 
