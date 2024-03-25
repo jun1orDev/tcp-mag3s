a7ccb1075fea4ce849b64a001fee52f6efb0e1d6
Jun1orDev <junior.araujo@mag3s.com>
2024-03-25 20:24:39 -0300

    configurações iniciais para utlização do pwa

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,11 @@
 <template>
 	<div>
+		<NuxtPwaManifest />
 		<NuxtPage :page-key="route => route.fullPath" />
 	</div>
-
+		<button @click="useNuxtApp().$pwa.install()">
+			Install {{ useNuxtApp().$pwa?.isPWAInstalled }}
+		</button>
 	<UNotifications />
 </template>
 
